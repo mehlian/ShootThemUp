e43de556e0a0bc32518dbf33228e61e5a463ff19
mehlian <xpilarz0@gmail.com>
2018-09-15 19:32:47 +0200

    [refactoerd] create function;

--- game.js
+++ game.js
@@ -15,78 +15,18 @@ BasicGame.Game.prototype = {
 
   create: function () {
 
-    this.sea = this.add.tileSprite(0, 0, 800, 600, 'sea');
-
-    this.player = this.add.sprite(400, 550, 'player');
-    this.player.anchor.setTo(0.5, 0.5);
-    this.player.animations.add('fly', [0, 1, 2], 20, true);
-    this.player.play('fly');
-    this.physics.enable(this.player, Phaser.Physics.ARCADE);
-    this.player.speed = 300;
-    this.player.body.collideWorldBounds = true;
-    // 20x20 pixel hitbox, centered a little bit higher than the center
-    this.player.body.setSize(20, 20, 0, -5);
-
-    this.enemyPool = this.add.group();
-    this.enemyPool.enableBody = true;
-    this.enemyPool.physicsBodyType = Phaser.Physics.ARCADE;
-    this.enemyPool.createMultiple(50, 'greenEnemy');
-    this.enemyPool.setAll('anchor.x', 0.5);
-    this.enemyPool.setAll('anchor.y', 0.5);
-    this.enemyPool.setAll('outOfBoundsKill', true);
-    this.enemyPool.setAll('checkWorldBounds', true);
-
-    // Ser the animation for each sprite
-    this.enemyPool.forEach(function (enemy) {
-      enemy.animations.add('fly', [0, 1, 2], 20, true);
-    });
-
-    this.nextEnemyAt = 0;
-    this.enemyDelay = 1000;
-
-    // Add an empty sprite group into our game
-    this.bulletPool = this.add.group();
-
-    // Enable physics to the whole sprite group
-    this.bulletPool.enableBody = true;
-    this.bulletPool.physicsBodyType = Phaser.Physics.ARCADE;
-
-    // Add 100 'bullet' sprites in the group.
-    // By default this uses the first frame of the sprite sheet and
-    // sets the initial state as non-existing (i.e. killed/dead)
-    this.bulletPool.createMultiple(100, 'bullet');
-
-    // Sets anchor of all sprites
-    this.bulletPool.setAll('anchor.x', 0.5);
-    this.bulletPool.setAll('anchor.y', 0.5);
-
-    // Automatically kill the bullet sprites when they go out of bounds
-    this.bulletPool.setAll('outOfBoundsKill', true);
-    this.bulletPool.setAll('checkWorldBounds', true);
-
-    this.nextShootAt = 0;
-    this.shotDelay = 100;
-
-    this.explosionPool = this.add.group();
-    this.explosionPool.enableBody = true;
-    this.explosionPool.physicsBodyType = Phaser.Physics.ARCADE;
-    this.explosionPool.createMultiple(100, 'explosion');
-    this.explosionPool.setAll('anchor.x', 0.5);
-    this.explosionPool.setAll('anchor.y', 0.5);
-    this.explosionPool.forEach(function (explosion) {
-      explosion.animations.add('boom');
-    })
+    this.setupBackground();
+    this.setupPlayer();
+    this.setupEnemies();
+    this.setupBullets();
+    this.setupExplosions();
+    this.setupText();
 
     this.cursors = this.input.keyboard.createCursorKeys();
-
-    this.instructions = this.add.text(400, 500, 'Use Arrow Keys to Move, Press Z to Fire\n' + 'Tapping/clicking does both', { font: '20px monospace', fill: '#fff', align: 'center' });
-    this.instructions.anchor.setTo(0.5, 0.5);
-    this.instExpire = this.time.now + 10000;
   },
 
   update: function () {
     //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
-    this.sea.tilePosition.y += 0.2;
 
     this.physics.arcade.overlap(this.bulletPool, this.enemyPool, this.enemyHit, null, this);
 
@@ -180,6 +120,88 @@ BasicGame.Game.prototype = {
   render: function () {
     // this.game.debug.body(this.bullet);
     // this.game.debug.body(this.enemy);
+  },
+
+  //
+  // create() - related functions
+  //
+  setupBackground: function () {
+    this.sea = this.add.tileSprite(0, 0, 800, 600, 'sea');
+    this.sea.autoScroll(0, 12);
+  },
+
+  setupPlayer: function () {
+    this.player = this.add.sprite(400, 550, 'player');
+    this.player.anchor.setTo(0.5, 0.5);
+    this.player.animations.add('fly', [0, 1, 2], 20, true);
+    this.player.play('fly');
+    this.physics.enable(this.player, Phaser.Physics.ARCADE);
+    this.player.speed = 300;
+    this.player.body.collideWorldBounds = true;
+    // 20x20 pixel hitbox, centered a little bit higher than the center
+    this.player.body.setSize(20, 20, 0, -5);
+  },
+
+  setupEnemies: function () {
+    this.enemyPool = this.add.group();
+    this.enemyPool.enableBody = true;
+    this.enemyPool.physicsBodyType = Phaser.Physics.ARCADE;
+    this.enemyPool.createMultiple(50, 'greenEnemy');
+    this.enemyPool.setAll('anchor.x', 0.5);
+    this.enemyPool.setAll('anchor.y', 0.5);
+    this.enemyPool.setAll('outOfBoundsKill', true);
+    this.enemyPool.setAll('checkWorldBounds', true);
+
+    // Ser the animation for each sprite
+    this.enemyPool.forEach(function (enemy) {
+      enemy.animations.add('fly', [0, 1, 2], 20, true);
+    });
+
+    this.nextEnemyAt = 0;
+    this.enemyDelay = 1000;
+  },
+
+  setupBullets: function () {
+    // Add an empty sprite group into our game
+    this.bulletPool = this.add.group();
+
+    // Enable physics to the whole sprite group
+    this.bulletPool.enableBody = true;
+    this.bulletPool.physicsBodyType = Phaser.Physics.ARCADE;
+
+    // Add 100 'bullet' sprites in the group.
+    // By default this uses the first frame of the sprite sheet and
+    // sets the initial state as non-existing (i.e. killed/dead)
+    this.bulletPool.createMultiple(100, 'bullet');
+
+    // Sets anchor of all sprites
+    this.bulletPool.setAll('anchor.x', 0.5);
+    this.bulletPool.setAll('anchor.y', 0.5);
+
+    // Automatically kill the bullet sprites when they go out of bounds
+    this.bulletPool.setAll('outOfBoundsKill', true);
+    this.bulletPool.setAll('checkWorldBounds', true);
+
+    this.nextShootAt = 0;
+    this.shotDelay = 100;
+  },
+
+  setupExplosions: function () {
+    this.explosionPool = this.add.group();
+    this.explosionPool.enableBody = true;
+    this.explosionPool.physicsBodyType = Phaser.Physics.ARCADE;
+    this.explosionPool.createMultiple(100, 'explosion');
+    this.explosionPool.setAll('anchor.x', 0.5);
+    this.explosionPool.setAll('anchor.y', 0.5);
+    this.explosionPool.forEach(function (explosion) {
+      explosion.animations.add('boom');
+    });
+  },
+
+  setupText: function () {
+    this.instructions = this.add.text(400, 500, 'Use Arrow Keys to Move, Press Z to Fire\n' + 'Tapping/clicking does both', { font: '20px monospace', fill: '#fff', align: 'center' });
+    this.instructions.anchor.setTo(0.5, 0.5);
+    this.instExpire = this.time.now + 10000;
   },
 
   quitGame: function (pointer) {
